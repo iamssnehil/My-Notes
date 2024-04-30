272feff108762ca338940b41790407010449704f
iamssnehil <123894630+iamssnehil@users.noreply.github.com>
2024-04-30 21:33:29 +0530

    import viewGroup

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -22,6 +22,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
