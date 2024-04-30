f611714a4622b4a351c4395607f4ef3e8382b525
iamssnehil <123894630+iamssnehil@users.noreply.github.com>
2024-04-30 12:57:23 +0530

    onclick function

--- app/src/main/java/com/example/mynotes/MainActivity.java
+++ app/src/main/java/com/example/mynotes/MainActivity.java
@@ -39,7 +39,17 @@ public class MainActivity extends AppCompatActivity {
 
         contentEditText = findViewById(R.id.contentEditText);
         ImageView imageView = findViewById(R.id.imageView);
-
+        //button click to show speech to text dialog
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //intent to show speech to text dialog
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi Speak Something");
+                }
+        });
         notesContainer = findViewById(R.id.notesContainer);
         Button saveButton = findViewById(R.id.saveButton);
 
